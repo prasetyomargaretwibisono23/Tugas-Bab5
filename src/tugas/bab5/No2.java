6160e9cf5fd68989b9ff60d820cd971bcdbf8357
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-04-27 20:30:40 +0700

    edit main class no2

--- src/tugas/bab5/No2.java
+++ src/tugas/bab5/No2.java
@@ -1,5 +1,7 @@
 package bab5;
+
 import java.util.Scanner;
+
 public class No2 {
     public static void main(String[] args) {
         Jaket jak = new Jaket();
@@ -13,6 +15,18 @@ public class No2 {
         System.out.println("4.keluar");
         System.out.print("Masukkan pilihan anda = ");
         pilih = in.nextInt();
-
-    
+        switch(pilih){
+            case 1 : System.out.println("Masukkan jumlah jaket yang dibeli =");
+                pilih = in.nextInt();    
+                jak.hargajaket1(pilih);break;
+            case 2 : System.out.println("Masukkan jumlah jaket yang dibeli =");
+                pilih = in.nextInt();
+                jak.hargajaket2(pilih);break;
+            case 3 : System.out.println("Masukkan jumlah jaket yang dibeli =");
+                pilih = in.nextInt();
+                jak.hargajaket3(pilih);break;
+        }
+        }while (pilih   !=4);
+        System.out.println("=> terima kasih sudah membeli !!! <=");
+    }
 }
