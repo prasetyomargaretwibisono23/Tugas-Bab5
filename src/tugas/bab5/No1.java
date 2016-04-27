fd156be898cc47f3b3632c7776f820c864685302
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-04-27 19:36:09 +0700

    edit main kelas no 1

---
+++ src/tugas/bab5/No1.java
@@ -0,0 +1,23 @@
+package bab5;
+import java.util.Scanner;
+public class No1 {
+    public static void main(String[] args) {
+        Menghitung angka = new Menghitung();
+        Scanner in = new Scanner(System.in);
+        int n;
+        do{
+        System.out.println("Menu");
+        System.out.println("1.Perkalian");
+        System.out.println("2.Pembagian");
+        System.out.println("3.Penumlahan");
+        System.out.println("4.Pengurangan");
+        System.out.println("5.Mensederhanakan");
+        System.out.print("Masukkan Urutan = ");
+        n = in.nextInt();
+        switch(n){
+            
+        }
+        }while (n  !=0);
+        System.out.println("Terima kasih sudah mencoba !!!");
+    }
+}
