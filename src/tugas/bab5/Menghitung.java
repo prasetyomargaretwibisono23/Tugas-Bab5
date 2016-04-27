8164afe490533cedb598dfe02b33906668101801
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-04-27 19:35:48 +0700

    edit kelas method no1

---
+++ src/tugas/bab5/Menghitung.java
@@ -0,0 +1,29 @@
+package bab5;
+public class Menghitung {
+    private int perkalian;
+    private int pembagian;
+    private int sederhana;
+    private static int penjumlahan;
+    private static int pengurangan;
+    public int hasil;
+    
+    public void pekalian(int n1,int n2 ){
+    hasil= n1 * n2;
+    System.out.println("Hasil perkalian A * B adalah "+hasil);
+    }
+    public void pembagian(int n1,int n2){
+    hasil= n1 * n2;
+    System.out.println("Hasil pembagian A / B adalah "+hasil);
+    }
+    public void penjumlahan(int n1,int n2){
+    hasil= n1 * n2;
+    System.out.println("Hasil penjumlahan A + B adalah "+hasil);
+    }
+    public void pengurangan(int n1,int n2){
+    hasil= n1 * n2;
+    System.out.println("Hasil pengurangan A - B adalah "+hasil);
+    }
+    public void sederhana(int n1,int n2){
+    System.out.println("Hasil pemangkatan A ^ B adalah "+Math.pow(n1,n2));
+    }
+}
