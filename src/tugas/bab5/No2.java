93e7a7eff4b7aa26044f2a95db46937412fff7e5
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-04-27 20:06:05 +0700

    edit main kelas no 2

---
+++ src/tugas/bab5/No2.java
@@ -0,0 +1,18 @@
+package bab5;
+import java.util.Scanner;
+public class No2 {
+    public static void main(String[] args) {
+        Jaket jak = new Jaket();
+        int pilih;
+        do{
+        Scanner in = new Scanner(System.in);
+        System.out.println("Menu Jaket");
+        System.out.println("1.Jaket kain");
+        System.out.println("2.Jaket Jeans");
+        System.out.println("3.Jaket Kulit");
+        System.out.println("4.keluar");
+        System.out.print("Masukkan pilihan anda = ");
+        pilih = in.nextInt();
+
+    
+}
